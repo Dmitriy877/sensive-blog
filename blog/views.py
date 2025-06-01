60f724189ce2b06aed92742e976fe6df9e7ecc56
Dmitriy877 <dmitriybukin05@gmail.com>
2025-06-01 21:50:04 +0300

    Delete useless code
    Delete useless code

--- blog/views.py
+++ blog/views.py
@@ -4,30 +4,11 @@ from django.db.models import Count
 from django.db.models import Prefetch
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def filter_posts_by_year(posts_query, year):
     posts_at_year = (
         posts_query.filter(published_at__year=year).order_by('published_at')
     )
     return posts_at_year
-
-
-def serialize_post(post):
-    tags = post.tags.all()
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in tags],
-        'first_tag_title': tags[0].title,
-    }
 
 
 def serialize_post_optimized(post):
@@ -41,13 +22,6 @@ def serialize_post_optimized(post):
         'slug': post.slug,
         'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
 
